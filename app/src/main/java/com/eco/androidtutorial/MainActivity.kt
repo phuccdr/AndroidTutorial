63da5ceba70dd4a3521ceb2a741eecdd6132419f
phucfrovis <phucfrovis@gmail.com>
2025-10-09 17:28:05 +0700

    push code 09-10

--- app/src/main/java/com/eco/androidtutorial/MainActivity.kt
+++ app/src/main/java/com/eco/androidtutorial/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.Lifecycle
 
 private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +22,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Log.d(TAG, "onCreate")
+        Log.d(TAG, "Khoi tao cac thanh phan chinh, bind data vào list, gan layout")
         val button = findViewById<TextView>(R.id.btn_nav)
         button.setOnClickListener {
             val intent = Intent(this, SecondaryActivity::class.java)
@@ -30,26 +33,31 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         Log.d(TAG, "onStart")
+        Log.d(TAG, "Activity trở nên có thể nhìn thấy được")
         super.onStart()
     }
 
     override fun onResume() {
         Log.d(TAG, "onResume")
+        Log.d(TAG, "Activiy trở nên tương tác được")
         super.onResume()
     }
 
     override fun onPause() {
         Log.d(TAG, "onPause")
+        Log.d(TAG, "Activity bị mất focus")
         super.onPause()
     }
 
     override fun onStop() {
         Log.d(TAG, "onStop")
+        Log.d(TAG, "Activity không còn có thể nhìn thấy được")
         super.onStop()
     }
 
     override fun onDestroy() {
         Log.d(TAG, "onDestroy")
+        Log.d(TAG, "Activity bị hủy")
         super.onDestroy()
     }
 
--- app/src/main/java/com/eco/androidtutorial/SecondaryActivity.kt
+++ app/src/main/java/com/eco/androidtutorial/SecondaryActivity.kt
@@ -2,22 +2,50 @@ package com.eco.androidtutorial
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.OnLifecycleEvent
 
 private const val TAG = "SecondaryActivity"
+private lateinit var cameraComponent: CameraComponent
+
 class SecondaryActivity : AppCompatActivity() {
+    lateinit var textView: TextView
+    var gameState: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        Log.d(TAG, "onCreate")
         setContentView(R.layout.activity_secondary)
+        gameState = savedInstanceState?.getString("GAME_STATE_KEY")
+        textView = findViewById(R.id.textView)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        cameraComponent = CameraComponent(this.lifecycle)
+        lifecycle.addObserver(cameraComponent)
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        textView.text = savedInstanceState.getString("TEXT_STATE_KEY")
+        Log.d(TAG, "onRestoreInstanceState lay du lieu duoc luu")
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run {
+            putString("GAME_STATE_KEY", gameState)
+            putString("TEXT_STATE_KEY", textView.text.toString())
+        }
+        Log.d(TAG, "onSaveInstanceState lưu dữ liệu tạm thời.")
+        super.onSaveInstanceState(outState)
     }
 
     override fun onStart() {
@@ -26,10 +54,10 @@ class SecondaryActivity : AppCompatActivity() {
 
     }
 
-    override fun onResume(){
+    override fun onResume() {
         Log.d(TAG, "onResume")
         super.onResume()
-
     }
+
 
 }
